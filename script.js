415ca2d586c2d9885220bd43c956fd63b9e2d981
er2525g-source <er2525g@gmail.com>
2025-09-27 03:33:07 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,319 @@
+// 冷やかしモンスター
+const hiyakashiData = [
+  {
+    name: "ズミえもん",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/hi1.png"
+  },
+  {
+    name: "古賀広起",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/hi2.png"
+  },
+  {
+    name: "やの",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/hi3.png"
+  },
+  {
+    name: "真道",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/hi4.png"
+  },
+  {
+    name: "shoma",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/hi5.png"
+  },
+  {
+    name: "たくみ",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/hi6.png"
+  },
+    {
+    name: "島根スサノオマジ",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/hi7.png"
+  },  
+  {
+    name: "シオン",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/hi8.png"
+  },  
+  {
+    name: "たぁ",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/hi9.png"
+  },  
+  {
+    name: "KO",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/hi10.png"
+  },  
+  {
+    name: "光造",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h11.png"
+  },  
+  {
+    name: "前田裕二",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h12.png"
+  },  
+  {
+    name: "teru",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h13.png"
+  },  
+  {
+    name: "たくや",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h14.png"
+  },  
+  {
+    name: "金城",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h15.png"
+  },  
+  {
+    name: "kota",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h16.png"
+  },  
+  {
+    name: "ユウジ",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h17.png"
+  },  
+  {
+    name: "り",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h18.png"
+  },  
+  {
+    name: "あ",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h19.png"
+  },  
+  {
+    name: "章",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h20.png"
+  },  
+  {
+    name: "高野",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h21.png"
+  },  
+  {
+    name: "ためちゃん",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h22.png"
+  },  
+  {
+    name: "にゃん汰",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h23.png"
+  },  
+  {
+    name: "アサイ",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h24.png"
+  },  
+  {
+    name: "晴",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h25.png"
+  },  
+  {
+    name: "韋駄天",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h26.png"
+  },  
+  {
+    name: "らいく",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h27.png"
+  },  
+  {
+    name: "O.lkumu",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h28.png"
+  },  
+  {
+    name: "fumi",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h29.png"
+  },  
+  {
+    name: "おかだ☆",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h30.png"
+  },  
+  {
+    name: "なお",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h31.png"
+  },  
+  {
+    name: "YUYASHIN",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h32.png"
+  },  
+  {
+    name: "りんた",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h33.png"
+  },  
+  {
+    name: "YH",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h34.png"
+  },  
+  {
+    name: "康佑",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h35.png"
+  },
+  {
+    name: "祐樹",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h36.png"
+  },  
+  {
+    name: "tadamori",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h37.png"
+  },  
+  {
+    name: "のぐっちゃん",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h38.png"
+  },  
+  {
+    name: "TOKIYA",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h39.png"
+  },  
+  {
+    name: "詩音",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h40.png"
+  },  
+  {
+    name: "闘魂",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h41.png"
+  },  
+  {
+    name: "じゅん",
+    type: "不明",
+    detail: "古代の冷やかし",
+    image: "images/h42.png"
+  },  
+  {
+    name: "あべゆうた",
+    type: "野球アイコン",
+    detail: "冷やかしの極み",
+    image: "images/h43.png"
+  }
+];
+
+// 用済みモンスター
+const yozumiData = [
+  {
+    name: "例",
+    price: "2000円",
+    detail: "不明",
+    image: "images/"
+  },
+  {
+    name: "あべゆうた",
+    price: "500円",
+    detail: "貧乏らしい",
+    image: "images/h43.png"
+  }
+];
+
+// 検索処理
+function searchMonster(type) {
+  const input = document.getElementById("searchInput").value;
+  const resultsDiv = document.getElementById("results");
+  resultsDiv.innerHTML = "";
+
+  const data = type === "hiyakashi" ? hiyakashiData : yozumiData;
+  const filtered = data.filter(m => m.name.includes(input));
+
+  if (filtered.length === 0) {
+    resultsDiv.innerHTML = "<p>該当の人物は見つかりませんでした。</p>";
+    return;
+  }
+
+  filtered.forEach(m => {
+    const card = document.createElement("div");
+    card.className = "monster-card";
+
+    if (type === "hiyakashi") {
+      // 冷やかし
+      card.innerHTML = `
+        <img src="${m.image}" alt="${m.name}">
+        <div>
+          <h3>${m.name} (タイプ: ${m.type})</h3>
+          <p>${m.detail}</p>
+        </div>
+      `;
+    } else {
+      // 用済み
+      card.innerHTML = `
+        <img src="${m.image}" alt="${m.name}">
+        <div>
+          <h3>${m.name} (金額: ${m.price})</h3>
+          <p>${m.detail}</p>
+        </div>
+      `;
+    }
+
+    resultsDiv.appendChild(card);
+  });
+}
